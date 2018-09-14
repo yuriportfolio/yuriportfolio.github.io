eb6fc1da0a8189ca642f55541c38f5520e720964
yuriportfolio <gagarinloops@gmail.com>
2018-09-14 07:25:31 +0300

    FURNITURE

---
+++ html5/resources/story.js
@@ -0,0 +1,776 @@
+var story = {
+"docName": "gglgo",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"pages": [
+{
+"image": "desktop_hd_copy_5.png",
+"image2x": "desktop_hd_copy_5@2x.png",
+"width": 1620,
+"height": 1395,
+"title": "Desktop HD Copy 5",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    862,
+    14,
+    884,
+    40
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    862,
+    14,
+    884,
+    40
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    903,
+    14,
+    925,
+    40
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    903,
+    14,
+    925,
+    40
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    781,
+    14,
+    803,
+    40
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    781,
+    14,
+    803,
+    40
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    822,
+    14,
+    844,
+    40
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    822,
+    14,
+    844,
+    40
+   ],
+   "page": 4
+  }
+]}
+,{
+"image": "desktop_hd_copy_4.png",
+"image2x": "desktop_hd_copy_4@2x.png",
+"width": 1670,
+"height": 5070,
+"title": "Desktop HD Copy 4",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1328,
+    67,
+    1517,
+    128
+   ],
+   "url": "https://yuriportfolio.github.io/index.html"
+  }
+,{
+  "rect": [
+    1446,
+    671,
+    1451,
+    682
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    1446,
+    671,
+    1451,
+    682
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    1029,
+    434,
+    1055,
+    460
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    1029,
+    434,
+    1055,
+    460
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    1077,
+    434,
+    1103,
+    460
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    1077,
+    434,
+    1103,
+    460
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    934,
+    434,
+    960,
+    460
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    934,
+    434,
+    960,
+    460
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    982,
+    434,
+    1008,
+    460
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    982,
+    434,
+    1008,
+    460
+   ],
+   "page": 4
+  }
+]}
+,{
+"image": "desktop_hd_copy_3.png",
+"image2x": "desktop_hd_copy_3@2x.png",
+"width": 1670,
+"height": 5070,
+"title": "Desktop HD Copy 3",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1330,
+    67,
+    1519,
+    128
+   ],
+   "url": "https://yuriportfolio.github.io/index.html"
+  }
+,{
+  "rect": [
+    1543,
+    671,
+    1548,
+    682
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    1543,
+    671,
+    1548,
+    682
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    1446,
+    671,
+    1451,
+    682
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    1446,
+    671,
+    1451,
+    682
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    1029,
+    434,
+    1055,
+    460
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    1029,
+    434,
+    1055,
+    460
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    1124,
+    434,
+    1150,
+    460
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    1124,
+    434,
+    1150,
+    460
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    934,
+    434,
+    960,
+    460
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    934,
+    434,
+    960,
+    460
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    982,
+    434,
+    1008,
+    460
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    982,
+    434,
+    1008,
+    460
+   ],
+   "page": 4
+  }
+]}
+,{
+"image": "desktop_hd_copy_2.png",
+"image2x": "desktop_hd_copy_2@2x.png",
+"width": 1670,
+"height": 5070,
+"title": "Desktop HD Copy 2",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1326,
+    69,
+    1515,
+    130
+   ],
+   "url": "https://yuriportfolio.github.io/index.html"
+  }
+,{
+  "rect": [
+    1543,
+    671,
+    1548,
+    682
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    1543,
+    671,
+    1548,
+    682
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    1446,
+    671,
+    1451,
+    682
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    1446,
+    671,
+    1451,
+    682
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    939,
+    434,
+    965,
+    460
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    939,
+    434,
+    965,
+    460
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1079,
+    434,
+    1105,
+    460
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    1079,
+    434,
+    1105,
+    460
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    986,
+    434,
+    1012,
+    460
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    986,
+    434,
+    1012,
+    460
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    1124,
+    434,
+    1150,
+    460
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    1124,
+    434,
+    1150,
+    460
+   ],
+   "page": 1
+  }
+]}
+,{
+"image": "desktop_hd.png",
+"image2x": "desktop_hd@2x.png",
+"width": 1670,
+"height": 5070,
+"title": "Desktop HD",
+'type': 'overlay',
+'overlayShadow': 0,
+"links": [
+{
+  "rect": [
+    88,
+    4208,
+    836,
+    4781
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    88,
+    4208,
+    836,
+    4781
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    1309,
+    66,
+    1498,
+    127
+   ],
+   "url": "https://yuriportfolio.github.io/index.html"
+  }
+,{
+  "rect": [
+    974,
+    434,
+    1000,
+    460
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    974,
+    434,
+    1000,
+    460
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    1024,
+    434,
+    1050,
+    460
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    1024,
+    434,
+    1050,
+    460
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    1073,
+    434,
+    1099,
+    460
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    1073,
+    434,
+    1099,
+    460
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    1121,
+    434,
+    1147,
+    460
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    1121,
+    434,
+    1147,
+    460
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    1519,
+    673,
+    1524,
+    684
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    1519,
+    673,
+    1524,
+    684
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    1421,
+    673,
+    1426,
+    684
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1421,
+    673,
+    1426,
+    684
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    934,
+    434,
+    960,
+    460
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    934,
+    434,
+    960,
+    460
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    1027,
+    434,
+    1053,
+    460
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    1027,
+    434,
+    1053,
+    460
+   ],
+   "page": 3
+  }
+]}
+,{
+"image": "desktop_hd_copy.png",
+"image2x": "desktop_hd_copy@2x.png",
+"width": 1670,
+"height": 5070,
+"title": "Desktop HD Copy",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1328,
+    72,
+    1528,
+    133
+   ],
+   "url": "https://yuriportfolio.github.io/"
+  }
+,{
+  "rect": [
+    1539,
+    671,
+    1544,
+    682
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    1539,
+    671,
+    1544,
+    682
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    977,
+    434,
+    1003,
+    460
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    977,
+    434,
+    1003,
+    460
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    1027,
+    434,
+    1053,
+    460
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    1027,
+    434,
+    1053,
+    460
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    1076,
+    434,
+    1102,
+    460
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    1076,
+    434,
+    1102,
+    460
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    1124,
+    434,
+    1150,
+    460
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    1124,
+    434,
+    1150,
+    460
+   ],
+   "page": 1
+  }
+]}
+,{
+"image": "flow.png",
+"image2x": "flow@2x.png",
+"width": 8630,
+"height": 5350,
+"title": "flow",
+'type': 'regular',
+"links": [
+]}
+,{
+"image": "text_styles.png",
+"image2x": "text_styles@2x.png",
+"width": 820,
+"height": 1223,
+"title": "Text Styles",
+'type': 'regular',
+"links": [
+]}
+,{
+"image": "fonts.png",
+"image2x": "fonts@2x.png",
+"width": 820,
+"height": 300,
+"title": "Fonts",
+'type': 'regular',
+"links": [
+]}
+,{
+"image": "color_palette.png",
+"image2x": "color_palette@2x.png",
+"width": 820,
+"height": 1340,
+"title": "Color Palette",
+'type': 'regular',
+"links": [
+]}
+   ]
+,"resolutions": [2],
+"title": "gglgo",
+"highlightLinks": false
+}
